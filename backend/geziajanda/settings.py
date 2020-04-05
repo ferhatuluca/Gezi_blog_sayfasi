6ce62e2b8c46b87a117a0c0144b416b96e47fe39
Reber Ferhat Uluca <34395388+ferhatuluca@users.noreply.github.com>
2020-04-05 23:28:58 +0300

    Update settings.py

--- backend/geziajanda/settings.py
+++ backend/geziajanda/settings.py
@@ -28,7 +28,8 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:4200'
+    'http://localhost:4200',
+    'http://127.0.0.1:4200',
 ]
 
 # Application definition
